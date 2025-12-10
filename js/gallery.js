6fffd17989065516c6106f2cfc143a6414aa0744
Gurung58 <gurungsmriti873@gmail.com>
2025-12-09 22:38:01 -0800

    updated gallery and contacts

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,11 @@
-const filterButtons = document.querySelectorAll(".section span");
+    const filterButtons = document.querySelectorAll(".section span");
     const categories = document.querySelectorAll(".category");
+    const images = document.querySelectorAll(".gallery-grid img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+    const lightboxTitle = document.getElementById("lightbox-title");
+    const lightboxDesc = document.getElementById("lightbox-desc");
+    const closeBtn = document.querySelector(".lightbox .close");
 
     filterButtons.forEach(btn => {
         btn.addEventListener("click", () => {
@@ -21,4 +27,22 @@ const filterButtons = document.querySelectorAll(".section span");
             filterButtons.forEach(b => b.classList.remove("active"));
             btn.classList.add("active");
         });
+    });
+    images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.classList.add("active");
+        lightboxImg.src = img.src;
+        lightboxTitle.textContent = img.dataset.title;
+        lightboxDesc.textContent = img.dataset.desc;
+    });
+    });
+
+    closeBtn.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+    });
+
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox) {
+        lightbox.classList.remove("active");
+        }
     });
